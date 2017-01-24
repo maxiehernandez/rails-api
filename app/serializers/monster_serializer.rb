42cf69fc5060a15158550969694951d2b5b64380
maxiehernandez <maxinehernandez@hotmail.com>
2017-01-24 12:54:13 -0500

    added active attr

--- app/serializers/monster_serializer.rb
+++ app/serializers/monster_serializer.rb
@@ -1,3 +1,3 @@
 class MonsterSerializer < ActiveModel::Serializer
-  attributes :id, :name, :level
+  attributes :id, :name, :level, :active
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124165426) do
+ActiveRecord::Schema.define(version: 20170124174931) do
 
   create_table "monsters", force: :cascade do |t|
     t.string  "name"
     t.integer "level"
+    t.boolean "active"
   end
 
 end
